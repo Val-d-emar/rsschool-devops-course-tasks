81a7d29503fe874053e4b2161ef633e73695beb9
Val-d-emar <vhot@live.ru>
2025-06-12 15:54:00 +0400

    [feat] add all futures

---
+++ e2_backend.tf
@@ -0,0 +1,11 @@
+# Optional: an example of a simple EC2 resource for demonstration
+# You can remove this block if you do not need to deploy EC2 for TASK 1.
+#resource "aws_instance" "example_instance" {
+#  ami           = var.ami_id           # AMI ID From the variable
+#  instance_type = var.instance_type    # Type of specimen from the variable
+
+#  tags = {
+#    Name        = "Task1-TestInstance" # Tag for resource identification
+#    Environment = "DevOpsCourse"
+#  }
+#}
---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  backend "s3" {
+    # The name of the S3 bucket that will be used to store the Terraform state file.
+    # As part of Task #1, this bucket must be created manually before running Terraform
+    # but in the checks, a certain bucket must be created in the code, we will do it below...
+    bucket         = "mybucketterraformname0"
+    key            = "terraform.tfstate"
+    region = "eu-north-1" # Unfortunately variables are not allowed in this section.
+    encrypt        = true
+    # dynamodb_table = var.lock_table #deprecated
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
